e940e7fd403248fefcc64ef1e2df2b0f31fee982
Aayush <rajkarnikaraayush@gmail.com>
2024-01-23 22:23:25 +0545

    Fibonacci complete

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -1,10 +1,15 @@
 package recursion;
 
 public class Fibonacci {
-    int fibonacci(){
-        
+    int fibonacci(int n){
+        if(n==0 || n==1){
+            return 1;
+        }else{
+            return fibonacci(n-2)+fibonacci(n-1);
+        }
     }
     public static void main(String[] args) {
-        
+        Fibonacci f=new Fibonacci();
+        System.out.println(f.fibonacci(5));
     }
 }

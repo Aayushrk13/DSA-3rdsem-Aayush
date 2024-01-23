328d9bbc22770faac2947a0dff846e99ee7df313
Aayush <rajkarnikaraayush@gmail.com>
2024-01-23 23:54:03 +0545

    Fibonacci commit

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
         if(n==0 || n==1){
             return 1;
         }else{
-            return fibonacci(n-2)+fibonacci(n-1);
+            return fibonacci(n-1)+fibonacci(n-2);
         }
     }
     public static void main(String[] args) {

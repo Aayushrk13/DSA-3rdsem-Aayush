c68cd2bd4405ef23f5f8ce12bd73d9a9e5bee391
Ayush13 <Nccspc@gmail.com>
2024-02-05 13:27:53 +0545

    Bubble sort"

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class Bubblesort {
+    void bubblesort(int arr[]){
+        int temp;
+        for(int i=0;i<arr.length;i++){
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i]>arr[j]) {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;   
+                }
+            }
+        }
+        System.out.println("Array in ascending sorted form is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        Bubblesort b=new Bubblesort();
+        System.out.println("Enter the size of the array:");
+        int size=scan.nextInt();
+        int []arr=new int [size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<size;i++){
+            arr[i]=scan.nextInt();
+        }
+        b.bubblesort(arr);
+    }
+}

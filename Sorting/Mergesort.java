01e933189a96c4d5ca0053fe81db5ba0e0650146
Ayush13 <Nccspc@gmail.com>
2024-03-06 13:59:51 +0545

    Merge sort

--- Sorting/Mergesort.java
+++ Sorting/Mergesort.java
@@ -11,18 +11,36 @@ public class Mergesort {
 
         for (int i = 0; i < left_array.length; i++) {
             left_array[i]=arr[i];
+            System.out.println(arr[i]);
         }
         for (int i = 0; i < right_array.length; i++) {
             right_array[i]=arr[mid_index+i];
+            System.out.println(arr[i]);
         }
         //Merge arrays
-        
+        int resarray[]=new int [arr.length];
+
+        int i=0;
+        int j=0;
+        int k=left_index;
+        while(i<subarray1_size && j<subarray2_size)
+        if(left_array[i]<right_array[j]){
+            resarray[k++]=left_array[i++];
+        }else{
+            resarray[k++]=right_array[j++];
+        }
+        while(i<subarray1_size){
+            resarray[k++]=left_array[i++];
+        }
+        while(j<subarray2_size){
+            resarray[k++]=right_array[j++];
+        }
     }
     void sort(int []arr,int left_index,int right_index){
         if(left_index<right_index){
             int mid_index=(left_index+right_index)/2;
             sort(arr, left_index, mid_index);
-            System.out.println(i++);
+            //System.out.println(i++);
             sort(arr, mid_index+1, right_index);
             merge(arr, left_index, right_index, mid_index);
         }

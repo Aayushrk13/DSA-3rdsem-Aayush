9a26d85dd90785319d6d456f5181912b4e52be4c
Aayush <rajkarnicar@gmail.com>
2024-03-07 08:53:49 +0545

    Merger sirt

--- Sorting/Mergesort.java
+++ Sorting/Mergesort.java
@@ -1,7 +1,7 @@
 package Sorting;
 
 public class Mergesort {
-    int i=0;
+    int temp[];
     void merge(int []arr,int left_index,int right_index,int mid_index){
         int subarray1_size=mid_index-left_index+1;
         int subarray2_size=right_index-mid_index;
@@ -15,14 +15,33 @@ public class Mergesort {
         for (int i = 0; i < right_array.length; i++) {
             right_array[i]=arr[mid_index+i];
         }
+        int []temp=new int[right_index-left_index+1];
         //Merge arrays
-        
+        int j=0; //for left 
+        int k=0; //for right
+        int l=0; //for temp array
+        while (j<subarray1_size && k<subarray2_size) {
+            if(left_array[j]<=right_array[k]){
+                arr[l++]=left_array[j++];
+            }else{
+                arr[l++]=right_array[k++];
+            }
+        }
+        while (j<subarray1_size) { //for left array
+            arr[l++]=left_array[j++];
+        }
+        while (k<subarray2_size) { //for right array
+            arr[l++]=right_array[k++];
+        }
+        int []resarray=new int [temp.length];
+        for (int i = 0; i < temp.length; i++) { //transfering to new array
+            resarray[i]=temp[i];
+        }
     }
     void sort(int []arr,int left_index,int right_index){
         if(left_index<right_index){
             int mid_index=(left_index+right_index)/2;
             sort(arr, left_index, mid_index);
-            System.out.println(i++);
             sort(arr, mid_index+1, right_index);
             merge(arr, left_index, right_index, mid_index);
         }
@@ -32,9 +51,15 @@ public class Mergesort {
         int right_index=arr.length-1;
         sort(arr, left_index, right_index);
     }
+    void printarray(int []arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
     public static void main(String[] args) {
         Mergesort ob=new Mergesort();
         int []arr={1,2,5,4,3};
         ob.mergesort(arr);
+        ob.printarray(arr);
     }
 }
